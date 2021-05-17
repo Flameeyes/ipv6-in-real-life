08ba2c32502fe439477220d6082e908a42f74705
Diego Elio Pettenò <flameeyes@flameeyes.com>
2021-05-17 19:17:13 +0100

    Make it easier to add data in the right structures.

--- ipv6_in_real_life/data_input.py
+++ ipv6_in_real_life/data_input.py
@@ -5,7 +5,7 @@
 import importlib.resources
 import itertools
 import json
-from typing import IO, Any, Dict, Iterable, Sequence
+from typing import IO, Any, Dict, Iterable, Iterator, Sequence
 
 from . import data_structures
 
@@ -22,15 +22,14 @@ def load_input_data(input_files: Sequence[IO[str]]) -> data_structures.Source:
     )
 
 
+def _all_json_entities_from_package() -> Iterator[Dict[str, Any]]:
+    for directory in importlib.resources.files("ipv6_in_real_life.data").glob("??"):
+        for packed_file in directory.glob("*.json"):
+            for json_entity in json.loads(packed_file.read_text()):
+                json_entity.setdefault("country", directory.name)
+                json_entity.setdefault("category", packed_file.stem)
+                yield json_entity
+
+
 def load_packaged_data() -> data_structures.Source:
-    return _source_from_json(
-        itertools.chain(
-            *(
-                json.loads(packed_file.read_text())
-                for directory in importlib.resources.files(
-                    "ipv6_in_real_life.data"
-                ).glob("??")
-                for packed_file in directory.glob("*.json")
-            )
-        )
-    )
+    return _source_from_json(_all_json_entities_from_package())
